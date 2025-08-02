07017c7a92e9182e1320a0190e214aef2ca5fe03
Stephane Moreel <stef.ksp@gmail.com>
2025-08-02 15:03:14 +0200

    Responsive mobile page accueil

--- src/Components/LogementsDetails.jsx
+++ src/Components/LogementsDetails.jsx
@@ -31,56 +31,65 @@ function LogementsDetails() {
           />
         </div>
 
-        <div className="container-title-location-host">
-          <div className="container-title-location">  
-            <div className="logement-title">{logement.title}</div>
-            <div className="logement-location"> {logement.location}</div>
-          </div>  
-          <div className="logement-host">
-            <div className="logement-host-name">{logement.host.name}</div>
-            <div className="logement-host-picture">
-              {" "}
-              <img
-                src={logement.host.picture}
-                alt={`photo de ${logement.host.name}`}
+        <div className="container-title-location-host-container-tags-rating">
+          
+          <div className="container-title-location-tags">
+
+            <div className="container-title-location">  
+              <div className="logement-title">{logement.title}</div>
+              <div className="logement-location"> {logement.location}</div>
+            </div> 
+
+            <div className="container-tags">
+              <div className="logement-tags">
+                {logement.tags.map((tag) => (
+                  <li key={tag}>{tag}</li>
+                ))}
+              </div>
+            </div>
+          </div>
+            <div className="container-host-rating">    
+
+              <div className="logement-host">
+                <div className="logement-host-name">{logement.host.name}</div>
+                <div className="logement-host-picture">
+                  {" "}
+                  <img
+                    src={logement.host.picture}
+                    alt={`photo de ${logement.host.name}`}
+                  />
+                </div>
+              </div>  
+                <div className="logement-rating">
+                  <Rating value={logement.rating} />
+                </div>
+              
+            </div>  
+          
+          </div>
+          <div className="container-description-equipments">
+            <div className="logement-description">
+              <Collapse
+                title="Description"
+                content={logement.description}
+                className="collapse-description"
               />
             </div>
-          </div>  
-        </div>
-
-        <div className="container-tags-rating">  
-          <div className="logement-tags">
-              {logement.tags.map((tag) => (
-                <li key={tag}>{tag}</li>
-              ))}
-          </div>
-          <div className="logement-rating">
-            <Rating value={logement.rating} />
+            <div className="logement-equipments">
+              <Collapse
+                title="Équipements"
+                content={
+                  <ul>
+                    {logement.equipments.map((equipment, index) => (
+                      <li key={index}>{equipment}</li>
+                    ))}
+                  </ul>
+                }
+              />
             </div>
-        </div>
-        
-        <div className="container-description-equipments">
-          <div className="logement-description">
-            <Collapse
-              title="Description"
-              content={logement.description}
-              className="collapse-description"
-            />
           </div>
-          <div className="logement-equipments">
-            <Collapse
-              title="Équipements"
-              content={
-                <ul>
-                  {logement.equipments.map((equipment, index) => (
-                    <li key={index}>{equipment}</li>
-                  ))}
-                </ul>
-              }
-            />
-          </div>
-        </div>
-      </div>  
+          
+      </div>
       <Footer />
     </div>
   );
--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -14,7 +14,7 @@ function Error(){
       <div className="error-page">
         <h1 className="error-title">404</h1>
         <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
-        <NavLink to="/" className="home-link">Retourner sur la page d’accueil</NavLink>
+        <NavLink to="/" className="home-navlink">Retourner sur la page d’accueil</NavLink>
       </div>
       <Footer />
       </div>
